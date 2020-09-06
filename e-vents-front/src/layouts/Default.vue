c12bfe5f5e3466b1a22e110e1adf3a4a9a719f42
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-09-06 14:11:24 +0500

    added gridsome and vuetify

---
+++ e-vents-front/src/layouts/Default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app class="layout">
+    <v-app-bar color="blue" app dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>E-vents</v-toolbar-title>
+      <v-text-field
+        placeholder="Search"
+        class="ml-8"
+        style="max-width:350px"
+        outlined
+        rounded
+        dense
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn small elevation color>Create Event</v-btn>
+    </v-app-bar>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
---
+++ e-vents-front/src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    <v-container>
+      <v-row>
+        <v-col sm="6" offset="3">
+          <v-tabs v-model="value">
+            <v-tab>item one</v-tab>
+            <v-spacer></v-spacer>
+            <v-tab>item two</v-tab>
+            <v-spacer></v-spacer>
+            <v-tab>item three</v-tab>
+          </v-tabs>
+        </v-col>
+      </v-row>
+    </v-container>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
